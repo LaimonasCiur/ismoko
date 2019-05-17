231f2ef21d489e2961c34bf30529d1202967e24b
Laimonas Ciurlionis <laimonas.ciurlionis@gmail.com>
2019-05-17 17:12:53 +0200

    fix fetched bug

--- public/js/main.js
+++ public/js/main.js
@@ -217,7 +217,7 @@ let markupGen = (dalykas, laikas, adresas, miestas) => {
   return markup;
 };
 
-fetch(`http://localhost:5000/tvarkarastis`)
+fetch(`http://ismoko.realpro.lt/tvarkarastis`)
   .then(result => {
     return result.json();
   })
@@ -241,26 +241,3 @@ fetch(`http://localhost:5000/tvarkarastis`)
     });
   })
   .catch(err => console.log(err));
-
-// function count() {
-//   array_elements = ['a', 'b', 'c', 'd', 'e', 'a', 'b', 'c', 'f', 'g', 'h', 'h', 'h', 'e', 'a'];
-
-//   array_elements.sort();
-
-//   var current = null;
-//   var cnt = 0;
-//   for (var i = 0; i < array_elements.length; i++) {
-//     if (array_elements[i] != current) {
-//       if (cnt > 0) {
-//         document.write(current + ' comes --> ' + cnt + ' times<br>');
-//       }
-//       current = array_elements[i];
-//       cnt = 1;
-//     } else {
-//       cnt++;
-//     }
-//   }
-//   if (cnt > 0) {
-//     document.write(current + ' comes --> ' + cnt + ' times');
-//   }
-// }
